df8ca1d00af64c4a077371e040f2d2cfbb57d80b
Ukachi Charles <ukcharlies@gmail.com>
2024-12-16 17:37:01 +0100

    setting up my react environment was a little trobulesome cause i didnt the p flag when initiallizing the tailwind. But it has been sorted in react-102

---
+++ react-intro/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+
+function App() {
+ 
+  return (
+    <>
+      <h1 className=' text-right'>React Intro</h1>
+      <p className=' text-sm'>React is a JavaScript library for building user interfaces.</p>
+    </>
+  )
+}
+
+export default App
